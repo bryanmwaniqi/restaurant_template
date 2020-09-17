82b1290d494eeeea0366498d375153eb3291e3c8
bryanmwaniqi <bryanmwaniqi@gmail.com>
2020-09-17 11:01:38 +0300

    add blog,location & location sections and styling

--- js/script.js
+++ js/script.js
@@ -2,4 +2,19 @@ $(function(){
     $(window).scroll(function(){
         $('nav.navbar').toggleClass('scrolled', $(this).scrollTop() > 200)
     });
+        $(".owl-carousel").owlCarousel({
+        margin:10,
+        loop: true,
+        stagePadding:10,
+        items:2,
+        center:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            900:{
+                items:2
+            }
+        }
+    });
 });
